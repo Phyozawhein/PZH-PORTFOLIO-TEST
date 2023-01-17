7adc580e8abea97f940c2b7d6660e723e1619a80
Phyo Zaw Hein <phyozawhein1997@gmail.com>
2023-01-17 04:10:31 -0500

    Finished the tutorial: set up EmailJS and added an interactive map on Contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,30 @@
 import './Contact.scss';
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect, useRef} from 'react';
 import Loader from 'react-loaders';
 import AnimatedLetter from '../AnimatedLetters/AnimatedLetters.js';
+import emailjs from '@emailjs/browser';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
 const Contact=()=>{
     const [letterClass,setLetterClass] = useState('text-animate')
-
+    const refForm  = useRef()
+    const sendEmail =(e)=>{
+        e.preventDefault();
+        emailjs.sendForm(
+            'default_service',
+            'template_iaidis1',
+            refForm.current,
+            'YHFnj6jm9kZ4YznL-'
+        ).then(
+            ()=>{
+                alert('Message successfully sent!')
+                window.location.reload(false);
+            },
+            (error)=>{
+                alert('Failed to send message ' + error.text)
+            }
+        )
+    }
     useEffect(()=>{
         setTimeout(()=>{
            setLetterClass('text-animate-hover')
@@ -28,7 +48,7 @@ const Contact=()=>{
                         You can contact me via form below ! 
                     </p>
                     <div className="contact-form">
-                        <form>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className="half">
                                     <input type="text" name ="name" placeholder="Name" required/>
@@ -48,6 +68,25 @@ const Contact=()=>{
                             </ul>
                         </form>
                     </div>
+                </div>
+                <div className="info-map">
+                    Phyo, Hein
+                    <br/>
+                    United States
+                    <br/>
+                    123 East 22nd,16A<br/>
+                    ABC City, ABC 12345 <br/>
+                    <span>phyozawhein1997@gmail.com</span>
+                </div>
+                <div className="map-wrap">
+                    <MapContainer center={[51.5072,0.1276]} zoom={13}>
+                        <TileLayer url ="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                        <Marker position={[51.5072,0.1276]}>
+                            <Popup>Phyo lives here !</Popup>
+                        
+                        </Marker>
+
+                    </MapContainer>
                 </div>
             </div>
             <Loader type="pacman"/>
